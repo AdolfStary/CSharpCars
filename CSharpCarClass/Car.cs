0102ced85e081d7b2d41456a81c3a9ee05e057ca
AdolfStary <adolf.stary@hotmail.com>
2020-10-22 17:49:39 -0600

    ADDED Motor class and made program work;

--- CSharpCarClass/Car.cs
+++ CSharpCarClass/Car.cs
@@ -10,7 +10,7 @@ namespace CSharpCarClass
         public string Model { get; set; }
         public string Color { get; set; }
         public int MaximumOccupancy { get; set; }
-        public double FuelEfficiency { get; set; }
+        public Motor Engine { get; set; }
         public Odometer Odometer { get; set; }
         public FuelTank FuelTank { get; set; }
 
@@ -20,7 +20,7 @@ namespace CSharpCarClass
             Model = "Boxster";
             Color = "Silver";
             MaximumOccupancy = 2;
-            FuelEfficiency = 10;
+            Engine = new Motor(10);
             Odometer = new Odometer(0);
             FuelTank = new FuelTank();
         }
@@ -30,21 +30,21 @@ namespace CSharpCarClass
             Model = model;
             Color = color;
             MaximumOccupancy = maxOccupancy;
-            FuelEfficiency = fuelEfficiency;
+            Engine = new Motor(fuelEfficiency);
             Odometer = new Odometer(odometer);
             FuelTank = new FuelTank(fuelTankCapacity, fuelLevel);
         }
 
         public void Drive(int km = 5)
         {
-            FuelTank.BurnFuel( km/FuelEfficiency );
+            FuelTank.BurnFuel( km/Engine.FuelEfficiency );
             Odometer.Increment( km );
         }
 
         public override string ToString()
         {
             string stringOdometer = "";
-            double availableKms =Math.Round(FuelTank.Level * FuelEfficiency, 0);
+            double availableKms =Math.Round(FuelTank.Level * Engine.FuelEfficiency, 0);
             string fuelStatus = FuelTank.Level == 0 ? "that has no fuel left." : $"that has enough fuel to travel {availableKms}KM.";
 
             for (int i = 1; i <= (6 - Odometer.Counter.ToString().Length); i++)
---
+++ CSharpCarClass/Motor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCarClass
+{
+    class Motor
+    {
+        public double FuelEfficiency { get; set; }
+
+        public Motor(double fuelEfficiency)
+        {
+            FuelEfficiency = fuelEfficiency;
+        }
+    }
+}
