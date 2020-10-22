977fcf7c76b4abc99b49743d4f7609924464d52e
AdolfStary <adolf.stary@hotmail.com>
2020-10-22 16:57:27 -0600

    ADDED FuelTank class;

--- CSharpCarClass/FuelTank.cs
+++ CSharpCarClass/FuelTank.cs
@@ -6,5 +6,48 @@ namespace CSharpCarClass
 {
     class FuelTank
     {
+        private double _capacity;
+        public double Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                _capacity = value;
+            }
+        }
+        private double _level;
+        public double Level 
+        {
+            get
+            {
+                return _level;
+            }
+            set
+            {
+                if (Level < 0)  throw new Exception ("Don't have enough fuel for the action.");
+                else if (Level > Capacity) _level = Capacity;
+                else _level = value;
+            }
+        }
+
+        public FuelTank(double capacity = 50, double level = 50)
+        {
+            Capacity = capacity;
+            Level = level;
+        }
+
+        public void BurnFuel(double liters = 0)
+        {
+            Level = Level - liters;
+        }
+
+        public void Fill(double liters = 0)
+        {
+            Level = Level + liters;
+        }
+
     }
 }
