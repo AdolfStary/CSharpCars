7c83414fc2ad1eee111bae72f4c225d8731178ba
AdolfStary <adolf.stary@hotmail.com>
2020-10-22 14:17:25 -0600

    CREATED project;

---
+++ CSharpCarClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
