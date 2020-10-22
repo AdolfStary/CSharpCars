b2d7fbe1e9c0748dc668a42de572a5870d2fa2b4
AdolfStary <adolf.stary@hotmail.com>
2020-10-22 14:49:24 -0600

    ADDED Odometer Class;

--- CSharpCarClass/Odometer.cs
+++ CSharpCarClass/Odometer.cs
@@ -6,6 +6,31 @@ namespace CSharpCarClass
 {
     class Odometer
     {
-        public int Counter { get; set; }
+        private int _counter;
+        public int Counter 
+        {
+            get 
+            {
+                return _counter;
+            } 
+            set
+            {
+                if ((_counter + value) > 999999)
+                {
+                    _counter = (_counter + value) - 999999;
+                }
+                else _counter = value;
+            }
+        }
+
+        public Odometer(int startingKm = 0)
+        {
+            Counter = startingKm;
+        }
+
+        public void Increment(int amount = 0)
+        {
+            Counter += amount;
+        }
     }
 }
